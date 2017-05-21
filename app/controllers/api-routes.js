70704ca472ca3a9f3ca7beb613e422fc97801292
Robert Lester <Lesters.robert@gmail.com>
2017-05-21 18:13:37 -0400

    user data now submits to user db

--- app/controllers/api-routes.js
+++ app/controllers/api-routes.js
@@ -5,6 +5,33 @@ const db = require('../models');
 
 module.exports = (app) => {
 
+    //Get route for getting all of the Users
+    app.get("/index", function(req, res) {
+        db.User.findAll({})
+            .then(function(data) {
+                //console.log(data);
+                const hbsObject = {
+                    foobar: data
+                };
+                //console.log(hbsObject);
+                res.render("index", hbsObject);
+            });
+    });
 
+    //POST route for saving a new User
+    app.post("/index", function(req, res) {
+        //console.log(req.body);
+        db.User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.username,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.password,
+            dob: req.body.dob
+        }).then(function() {
+            res.redirect("/");
+        });
+    });
 
 };
