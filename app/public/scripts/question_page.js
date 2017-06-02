7f694b238e4454fc3b1ecde6384b2983156a620f
Robert Lester <Lesters.robert@gmail.com>
2017-06-02 17:02:13 -0400

    polished the layouts

--- app/public/scripts/question_page.js
+++ app/public/scripts/question_page.js
@@ -4,23 +4,23 @@ function createQuestionPage() {
   const h1 = $('<h1>').addClass('text-center')
     .text('What\'s on Your Mind?').css('margin-bottom', '50px');
   const askAQuestion = $('<div>').addClass('row');
-  const quesCloud = $('<div>').addClass('col-lg-6 question-cloud');
+  const quesCloud = $('<div>').addClass('col-md-3 col-md-offset-1 text-center question-cloud');
   const quesLink = $('<a>').attr('href', '/app');
   const quesImg = $('<img>').attr('src', '../public/img/cloud_question.png')
-    .css('width', '300px').css('height', 'auto');
-  const quesDiv = $('<div>').addClass('col-lg-6 question');
+    .css('width', '400px').css('height', 'auto');
+  const quesDiv = $('<div>').addClass('col-md-5 col-md-offset-2 text-center question');
   const questionForm = $('<form>').attr('method', 'POST').attr('action', '/app')
     .attr('id', 'ask-a-question');
   const textDiv = $('<div>').addClass('form-group');
   const textInput = $('<textarea>').addClass('form-control')
-    .attr('rows', '5').attr('id', 'whatsOnYourMind').attr('name', 'question')
+    .attr('rows', '8').attr('id', 'whatsOnYourMind').attr('name', 'question').css('margin-top', '20px')
       .attr('placeholder', 'Enter your question ...');
   const submitBtn = $('<button>').text('Submit').attr('type', 'submit')
     .addClass('btn btn-default').css('float', 'right');
 
     const row = $('<div>').addClass('row');
     const backBtnDiv = $('<div>').addClass('col-lg-12');
-    const genCatBtn = $('<input>').attr('type', 'image').attr('src', '../public/img/Back.png').addClass('backbtn').attr('id', 'genCats').attr('data-name', 'button');
+    const genCatBtn = $('<input>').attr('type', 'image').attr('src', '../public/img/Back.png').addClass('backbtn').attr('id', 'genCats').attr('data-name', 'button').css('margin', '40px 0 0 50px');
 
   $('.wrapper').append(heading);
   heading.append(h1);
