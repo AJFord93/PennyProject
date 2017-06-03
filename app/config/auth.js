e12c92c4101defda3fc7c6dfda13bd32ae5e26c5
AJ Ford <AJCranford1019@gmail.com>
2017-06-03 09:12:04 -0400

    Passport Auth Update

--- app/config/auth.js
+++ app/config/auth.js
@@ -8,7 +8,7 @@ module.exports ={
     'facebookAuth': {
         'clientID':'1908951959394190', //PennyProject's facebook App ID
         'clientSecret':'e5976f905692903f91425021edecc392', //PennyProject's facebook AppSecret
-        'callbackURL':'http://localhost:8080/auth/facebook/callback'
+        'callbackURL':'http://pennyproject93.herokuapp.com/auth/facebook/callback'
 
      },
 
